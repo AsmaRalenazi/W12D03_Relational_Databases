e412bc70f8141af2caa01803fbc35c6a45afff3f
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-06-16 00:49:11 +0300

    solve 0. P3 Starter Code (MySQL)

---
+++ db/db.js
@@ -0,0 +1,22 @@
+const mysql=require("mysql2")
+require('dotenv').config();
+
+const bcrypt = require("bcrypt");
+
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+  });
+
+// connecting mysql
+connection.connect((err) => {
+	if (err) {
+	  console.error("error connecting: " + err.stack);
+	  return;
+	}
+	console.log("connected as id " + connection.threadId);
+  });
+  
+  module.exports = connection;
